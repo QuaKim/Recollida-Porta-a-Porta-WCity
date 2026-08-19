2a7068900a2a4ae75a0334d4613435297f5184af
QuaKim <121525213+QuaKim@users.noreply.github.com>
2026-08-19 13:26:12 +0200

    Initialize WCityCoordinator in async_setup_entry
    Added WCityCoordinator initialization and updated logging.

--- custom_components/wcity_pap/__init__.py
+++ custom_components/wcity_pap/__init__.py
@@ -3,6 +3,9 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN
+from .coordinator import WCityCoordinator
+
 _LOGGER = logging.getLogger(__name__)
 
 # Definimos las plataformas que soporta la integración
@@ -13,8 +16,14 @@ PLATFORMS: list[Platform] = [
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Configura la integración Obre la Porta a partir de una entrada."""
-    _LOGGER.debug("Cargando plataformas para Obre la Porta: %s", PLATFORMS)
+    """Configura la integración a partir de una entrada."""
+    _LOGGER.debug("Cargando coordinador y plataformas para Obre la Porta")
+    
+    # Inicializamos el coordinador y lo guardamos en hass.data
+    coordinator = WCityCoordinator(hass, entry)
+    await coordinator.async_config_entry_first_refresh()
+    
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     
     # Carga las entidades de sensor.py y calendar.py
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
@@ -24,4 +33,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Descarga las plataformas cuando se elimina o recarga la integración."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
