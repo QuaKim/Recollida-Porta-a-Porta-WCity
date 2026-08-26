816e715a27c681224ad136c8c6ce0a6444247e9d
QuaKim <121525213+QuaKim@users.noreply.github.com>
2026-08-26 23:41:19 +0200

    Delete custom_components/wcity_pap/const.py

--- custom_components/wcity_pap/const.py
+++
@@ -1,8 +0,0 @@
-DOMAIN = "wcity_pap"
-
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-
-URL_BASE = "https://obrelaporta.wcity.app"
-URL_API_ADRECA = f"{URL_BASE}/modules/WCITY/api/v1/usuario/adreca"
-URL_API_CALENDARI_AVUI = f"{URL_BASE}/modules/WCITY/api/v1/sector/calendari?que=avui"
